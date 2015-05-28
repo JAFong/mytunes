822f92ad57d81a1115a08c7382708239ed197cce
Hack Reactor Students <accounts@hackreactor.com>
2015-05-27 19:15:57 -0700

    complete enqueue, SongQueueView, LibraryView and AppView

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -1,7 +1,9 @@
 // SongQueue.js - Defines a backbone model class for the song queue.
 var SongQueue = Songs.extend({
 
+  // model: SongModel,
+
   initialize: function(){
-  }
+  },
 
 });
--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -1,11 +1,23 @@
 // SongQueueView.js - Defines a backbone view class for the song queue.
 var SongQueueView = Backbone.View.extend({
 
+  tagName: "table",
+
   initialize: function() {
+    this.render();
+    this.collection.on('add remove', function() {
+      this.render();
+    }, this)
   },
 
   render: function() {
-    return this.$el;
+    // possible detach for event handling
+
+    this.$el.html('<th>Song Queue</th>').append(
+      this.collection.map(function(song){
+        return new SongQueueEntryView({model: song}).render();
+      })
+    );
   }
 
 });
